8fbb1019dfbd7c639a2e5610c25e4bf6f6695087
ahmedkh67 <al.monster.games@gmail.com>
2025-03-10 21:59:47 +0300

    ef

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
 
-const ServiceCard = () => {
+
+type ServiceCardTypes = {
+    title : string , 
+    bio : string , 
+    link: string
+}
+
+function ServiceCard ({title , bio , link}: ServiceCardTypes ){
   return (
     <>
     <div className="ServiceCard_Container bg-zinc-900">
 
         <div className="ServiceCard_Content">
             <h1 className='text-center text-2xl font-bold text-white'>
-                تأجير أدوات الزينة و الديكور
+                {title}
             </h1>
             <p className='text-center text-lg text-white opacity-80'>
-            لدينا كل ما يضيف لمسة جمالية لمناسباتك
-
+                {bio}
             </p>
 
-            <Link href={"/Servc"}>
+            <Link href={link}>
                 <button className="flex mt-10 bg-black h-14 justify-center items-center px-20 rounded-md
                     transition-[0.3s] hover:bg-blue-500 border-0 border-white border-solid">
                         <h1 className='text-white font-bold text-xl'>
--- components/Servicesec.tsx
+++ components/Servicesec.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ServiceCard from './ServiceCard'
+import { ServicesCards } from '@/Constants'
 
 const Servicesec = () => {
   return (
   <>
     <div className="flex md:flex-row flex-col w-full justify-evenly items-center overflow-hidden bg-white py-[50px] flex-wrap">
-      <ServiceCard />
-      <ServiceCard />
-      <ServiceCard />
-      <ServiceCard />
-      <ServiceCard />
+      
+    {ServicesCards.map((item) => (
+      <ServiceCard title={item.title} bio={item.bio} link={item.link} key={item.title} />
+    ))}
       
     </div>
     
