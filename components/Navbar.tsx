73c926e147997eec0a4c2b8d97705b6a37cf6203
ahmedkh67 <al.monster.games@gmail.com>
2025-03-11 07:58:01 +0300

    fgh

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,17 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import { Navlist } from '@/Constants'
 import MenuIcon from '@/public/Menu 2 Icon.svg'
 import Image from 'next/image'
+import { useState } from 'react'
+import Logo from '@/public/Logo.jpg'
+import CloseIcon from '@/public/Closeicon.svg'
 
 const Navbar = () => {
+  const [open , setopen] = useState(false) ;
+
   return (
     <>
     <div className="NavBar z-30">
@@ -29,8 +36,42 @@ const Navbar = () => {
         </h1>
      </button>
 
-     <Image src={MenuIcon} alt='Menu list icon ' width={50} height={50} className='md:hidden flex' />
+     <Image src={MenuIcon} alt='Menu list icon ' width={50} height={50} className='md:hidden flex'
+     onClick={() => {setopen(!open)}}
+      />
 
+    </div>
+    <div className={`flex w-full bg-white h-full top-0 right-0 fixed z-40 
+      px-6 flex-col justify-start  pt-6 ${open? 'Menuinactive' : 'Menuactive'} *:`}>
+        <div className="flex w-full justify-between items-center ">
+          <Image src={Logo} alt={"logo img"} width={140} height={50} />
+          <Image src={CloseIcon} alt={"close icon"} width={25} height={25} onClick={() => {setopen(!open)}}  />
+        </div>
+        <div className="flex flex-col justify-start items-end">
+          <Link href={'././'} className='w-full' onClick={() => {setopen(!open)}} >
+            <div className="flex w-full py-4 justify-end ">
+              <h1 className='text-4xl text-end font-bold my-4' >
+              الرئيسية
+              </h1>
+            </div>
+          </Link>
+          <Link href={'/Projectspage'} className='w-full' onClick={() => {setopen(!open)}} >
+            <div className="flex w-full py-4 justify-end ">
+              <h1 className='text-4xl text-end font-bold my-4' >
+              أبرز أعمالنا
+              </h1>
+            </div>
+          </Link>
+          <Link href={'/Contacts'} className='w-full' onClick={() => {setopen(!open)}} >
+            <div className="flex justify-end w-full py-4">
+              <h1 className='text-4xl text-end font-bold my-4' >
+              تواصل معنا
+              </h1>
+            </div>
+          </Link>
+          
+          
+        </div>
     </div>
 
     </>
