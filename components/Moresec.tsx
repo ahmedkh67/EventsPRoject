48a3779720fa03acf8564d7236aba37571caea37
ahmedkh67 <al.monster.games@gmail.com>
2025-03-11 18:29:49 +0300

    ghg

--- components/Moresec.tsx
+++ components/Moresec.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import Morebox from './MoreBox';
 import { Moreboxes } from '@/Constants';
+import { motion } from 'motion/react';
 
 
 
@@ -12,10 +13,11 @@ const Moresec = () => {
     <div id='moresec' className="flex w-full flex-col md:flex-row bg-white md:justify-around items-start justify-around md:py-0 py-3">
       
       {Moreboxes.map((item) => (
+        <motion.div initial={{opacity: 0, scale: 0}} whileInView={{opacity:1 , scale: 1}}>
         
         <Morebox title={item.MoreBoxtitle} 
         bio={item.MoreBoxbio} srcs={item.MoreBoxsrc} key={item.MoreBoxtitle} />
-       
+       </motion.div>
       ))}
     
     </div>  
