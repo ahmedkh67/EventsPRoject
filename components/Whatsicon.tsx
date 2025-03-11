9895fb1db00d93e6857d5b89c67f8f0154be1081
ahmedkh67 <al.monster.games@gmail.com>
2025-03-11 08:53:41 +0300

    rfdfd

--- components/Whatsicon.tsx
+++ components/Whatsicon.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 const Whatsicon = () => {
   return (
     <>
-    <Link href={'https://wa.me/'}>
-    <div className="flex w-24 h-24 rounded-full p-2 bg-green-500 justify-center items-center fixed bottom-11 left-11">
+    <Link href={'https://wa.me/+966590029919'}>
+    <div className="flex z-40 w-24 h-24 rounded-full p-2 bg-green-500 justify-center items-center fixed bottom-11 left-11">
         <Image src={whatsappIcon} alt={'whatsapp icon'} width={100} height={60} />
     </div>
     </Link>
